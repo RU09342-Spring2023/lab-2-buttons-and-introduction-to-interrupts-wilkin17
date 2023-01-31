f963a35314095025c8161e5d903b9557fb86af70
wilkin17 <wilkin17@students.rowan.edu>
2023-01-31 17:09:10 -0500

    Added green LED and LED swapping
    When the button is pressed it swaps between which LED is blinking

--- 2 Interrupts and Interrupt Routines/Part2.c
+++ 2 Interrupts and Interrupt Routines/Part2.c
@@ -28,6 +28,8 @@ int main(void)
     P1DIR |= BIT0;                          // Set P1.0 to output direction
 
     // @TODO You need to add in the configuration for the Green LED
+    P6OUT &= ~BIT6;                         // Clear P6.6 output latch for a defined power-on state
+    P6DIR |= BIT6;                          // Set P1.0 to output direction
 
     P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
     P2REN |= BIT3;                          // P2.3 pull-up register enable
@@ -45,10 +47,14 @@ int main(void)
     while(1)
     {
         // @TODO You will need to modify this code to change between blinking the Red LED or the Green LED
-        if (ToggleEnable)
+        if (ToggleEnable){
             P1OUT ^= BIT0;                  // P1.0 = toggle
-        else
+            P6OUT &= ~BIT6;                 // Set P6.6 to 0
+        }
+        else{
             P1OUT &= ~BIT0;                 // Set P1.0 to 0
+            P6OUT ^= BIT6;                  // P6.6 = toggle
+        }
         __delay_cycles(100000);
     }
 }
